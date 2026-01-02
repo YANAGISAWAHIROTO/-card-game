f960f79ee3f70e3bf903e68338f3bbfcb4b519fc
柳澤大翔 <yhiroto465@gmail.com>
2026-01-02 21:19:00 +0900

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,8 +1,3 @@
-let currentDice = null;
-
 function rollDice() {
-  currentDice = Math.floor(Math.random() * 6) + 1;
-  document.getElementById("dice-result").textContent =
-    "出目：" + currentDice;
-  return currentDice;
+  return Math.floor(Math.random() * 6) + 1;
 }
